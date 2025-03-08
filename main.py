c6789fe34553868d8aff7007e65672e348d9ee22
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2025-03-08 04:15:06 -0800

    OOP1

---
+++ main.py
@@ -0,0 +1,8 @@
+class food:
+    def __init__ (self, name, nutrient):
+        self.name = name
+        self.nutrient = nutrient
+
+food1 = food("Rice", "Carbohydrate")
+food2 = food("Beans", "Protein")
+print("Hello, I am", food1.name, "and I am a", food1.nutrient)
